ee3fd430a76971d6a85712eecd2606bf7066fefa
Nicolas <nynox77@hotmail.fr>
2018-08-08 15:38:40 +0200

    test

--- src/controls/Link/index.js
+++ src/controls/Link/index.js
@@ -59,8 +59,9 @@ class Link extends Component {
 
   onChange = (action, title, target, targetOption) => {
     if (action === 'link') {
-      const links = linkify.match(target);
-      const linkifiedTarget = links && links[0] ? links[0].url : '';
+      // const links = linkify.match(target);
+      // const linkifiedTarget = links && links[0] ? links[0].url : '';
+      const linkifiedTarget = target;
       this.addLink(title, linkifiedTarget, targetOption);
     } else {
       this.removeLink();
